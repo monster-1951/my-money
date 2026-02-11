d3669a541c77e5758de9273d280aeb98bbbe5c6a
Shiva Shankar <shivashankarsince19@gmail.com>
2026-02-11 20:56:35 +0530

    80% of the UI for RecordInput Component is done

--- src/components/CreatePageComponents/CreatePage.tsx
+++ src/components/CreatePageComponents/CreatePage.tsx
@@ -1,29 +1,76 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { NavLink } from "react-router-dom";
 import type { record_type } from "../../types/types";
 import RecordInput from "./RecordInput";
-const CreateOrEditTransactioinPage = () => {
-    const [type,setType] = useState<record_type>("Expense")
-    const [amount] = useState(0)
-    const handleClick = (event:React.MouseEvent<HTMLButtonElement>) => {
-        setType(event.currentTarget.innerText as record_type)
-    }
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
+const CreateOrEditTransactioinPage = () => {
+  const [date, setDate] = useState<Date | null>(new Date());
+  const dateTimeInputRef = useRef<DatePicker | null>(null);
+  const [type, setType] = useState<record_type>("Expense");
+  const [amount] = useState(0);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setType(event.currentTarget.innerText as record_type);
+  };
+  const handleDateTimeSelectSelect = () => {
+    dateTimeInputRef.current?.setOpen(true);
+  };
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col min-h-dvh w-full sm:w-[80%] md:w-[40%] xl:w-[30%] mx-auto">
       {/* Cancel Save Button */}
-      <div className="flex justify-between p-5">
+      <div className="flex justify-between px-5 py-1 shrink-0">
         <NavLink to={"/"}>&#10005; CANCEL</NavLink>
         <button>&#10003; SAVE</button>
       </div>
 
-      <div className="flex justify-center space-x-3">
-        <button onClick={handleClick} className={`${type==="Income" ? 'font-bold': ''}`}>Income</button> <span>|</span>
-        <button onClick={handleClick} className={`${type==="Expense" ? 'font-bold': ''}`}>Expense</button> <span>|</span>
-        <button onClick={handleClick} className={`${type==="Transfer" ? 'font-bold': ''}`}>Transfer</button>
+      <div className="flex justify-center space-x-3 p-3">
+        <button
+          onClick={handleClick}
+          className={`${type === "Income" ? "font-bold" : ""}`}
+        >
+          Income
+        </button>{" "}
+        <span>|</span>
+        <button
+          onClick={handleClick}
+          className={`${type === "Expense" ? "font-bold" : ""}`}
+        >
+          Expense
+        </button>{" "}
+        <span>|</span>
+        <button
+          onClick={handleClick}
+          className={`${type === "Transfer" ? "font-bold" : ""}`}
+        >
+          Transfer
+        </button>
       </div>
-    <RecordInput record_type={type} amount={amount}/>
-
+      <RecordInput record_type={type} amount={amount} />
+        <DatePicker
+          selected={date}
+          onChange={(date: Date | null) => {
+            setDate(date);
+          }}
+          showTimeSelect
+          timeIntervals={15}
+          ref={dateTimeInputRef}
+          className="hidden"
+        />
+      <div className="flex justify-between py-2">
+        <input type="time" name="time" id="" hidden />
+        <button onClick={handleDateTimeSelectSelect} className="w-1/2 ">
+          {date?.toDateString()}
+        </button>{" "}
+        <span>|</span>
+        <button className="w-1/2 "
+          onClick={handleDateTimeSelectSelect}
+        >{`${date?.toLocaleTimeString("en-US", {
+          hour: "numeric",
+          minute: "2-digit",
+          hour12: true,
+        })}`}</button>
+      </div>
     </div>
   );
 };
--- src/components/CreatePageComponents/RecordInput.tsx
+++ src/components/CreatePageComponents/RecordInput.tsx
@@ -2,15 +2,56 @@ import { CiWallet } from "react-icons/ci";
 import type { record_type } from "../../types/types";
 import { IoPricetagOutline } from "react-icons/io5";
 import { PiBackspace } from "react-icons/pi";
-
+import { useState } from "react";
+import * as math from "mathjs";
 interface RecordInputProps {
   record_type: record_type;
-  amount:number
+  amount: number;
+}
+
+interface CalculatorButton {
+  value: string;
+  onClick: Function;
 }
 
 const RecordInput = (props: RecordInputProps) => {
+  const [amountString, setAmountString] = useState<string>("0");
+  const NumberClicked = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
+    console.log("clicked", e.currentTarget.innerText);
+    const value = e.currentTarget.innerText;
+    setAmountString((prev) => prev + value);
+    console.log(amountString);
+  };
+  const handleBackSpace = () => {
+    setAmountString((prev) => prev.slice(0, prev.length - 1));
+  };
+  const evaluate = () => {
+    const result = math.evaluate(amountString);
+    console.log(result);
+    setAmountString(result);
+  };
+  const calculator_buttons: CalculatorButton[] = [
+    { value: "+", onClick: NumberClicked },
+    { value: "7", onClick: NumberClicked },
+    { value: "8", onClick: NumberClicked },
+    { value: "9", onClick: NumberClicked },
+    { value: "-", onClick: NumberClicked },
+    { value: "4", onClick: NumberClicked },
+    { value: "5", onClick: NumberClicked },
+    { value: "6", onClick: NumberClicked },
+    { value: "*", onClick: NumberClicked },
+    { value: "1", onClick: NumberClicked },
+    { value: "2", onClick: NumberClicked },
+    { value: "3", onClick: NumberClicked },
+    { value: "/", onClick: NumberClicked },
+    { value: "0", onClick: NumberClicked },
+    { value: ".", onClick: NumberClicked },
+    { value: "=", onClick: evaluate },
+  ];
   return (
-    <div className="flex flex-col py-3">
+    <div className="flex flex-col py-3 flex-1">
       <>
         <div className="flex justify-around">
           <button className="w-full text-xs font-light">Account</button>
@@ -18,7 +59,7 @@ const RecordInput = (props: RecordInputProps) => {
             {props.record_type !== "Transfer" ? "Category" : "To"}
           </button>
         </div>
-        <div className="flex justify-around px-1 space-x-1 ">
+        <div className="flex justify-around px-1 py-2 space-x-1 font-semibold">
           <div className="w-full border flex justify-center rounded py-2 space-x-1">
             <CiWallet className="text-2xl" />
             <span>Account</span>
@@ -39,15 +80,42 @@ const RecordInput = (props: RecordInputProps) => {
       </>
 
       <>
-      <div className="p-1">
-        <textarea className="rounded border px-1 w-full h-[22vh] focus:border-0" placeholder="Add notes"/>
-      </div>
+        <div className="p-1 flex-1">
+          <textarea
+            className="rounded border px-1 w-full h-full min-h-5 resize-none focus:border-0"
+            placeholder="Add notes"
+          />
+        </div>
       </>
 
       <>
-      <div className="p-1">
-        <div className="flex rounded border px-1 w-full h-[8vh] focus:border-0  justify-end space-x-2" ><span className="font-semibold text-4xl my-auto">{props.amount}</span><PiBackspace className="text-2xl my-auto"/></div>
-      </div>
+        <div className="p-1">
+          <div className="flex rounded border px-1 w-full focus:border-0 justify-end gap-2 py-3">
+            <span className="font-semibold text-4xl">
+              {amountString}
+            </span>
+            <PiBackspace
+              className="text-2xl my-auto"
+              onClick={handleBackSpace}
+            />
+          </div>
+        </div>
+      </>
+
+      <>
+        <div className="p-1 grid grid-cols-4 flex-1 max-h-80">
+          {calculator_buttons.map((button) => (
+            <button
+              className=" rounded border"
+              onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+                button.onClick(e);
+              }}
+              key={button.value}
+            >
+              {button.value}
+            </button>
+          ))}
+        </div>
       </>
     </div>
   );
